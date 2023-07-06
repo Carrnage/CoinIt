ef1f588012e3cd8cd8926f43686907f9429b37aa
Quintin <Carrnage4@gmail.com>
2023-07-06 20:17:53 +1200

    Basic pinpad layout

--- Pages/Activities/Pin.js
+++ Pages/Activities/Pin.js
@@ -4,15 +4,35 @@ import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { Text, View, Button } from "react-native";
 import { styles } from "../../Stylesheets/AppStyle";
 
-export default function PinScreen({navigation}) {
+export default function PinScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.title}>CoinIt</Text>
       <StatusBar style="auto" />
       <View>
         <Text>Pin</Text>
-        <Button title='submit'
-        onPress={()=> navigation.navigate('Home')}/>
+        <View style={[{ flexDirection: "column" }]}>
+          <View style={[{ flexDirection: "row" }]}>
+            <Button title="7" />
+            <Button title="8" />
+            <Button title="9" />
+          </View>
+          <View style={[{ flexDirection: "row" }]}>
+            <Button title="4" />
+            <Button title="5" />
+            <Button title="6" />
+          </View>
+          <View style={[{ flexDirection: "row" }]}>
+            <Button title="1" />
+            <Button title="2" />
+            <Button title="3" />
+          </View>
+          <View style={[{ flexDirection: "row" }]}>
+            <Button title="<" />
+            <Button title="0" />
+            <Button title="=" onPress={() => navigation.navigate("Home")} />
+          </View>
+        </View>
       </View>
     </View>
   );
