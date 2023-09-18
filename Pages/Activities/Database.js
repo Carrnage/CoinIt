1b17f3f29b82eb7f46839f44ec0767d65db92241
hangreg1842@gmail.com <hangreg1842@gmail.com>
2023-09-19 10:40:46 +1200

    0.1

--- Pages/Activities/Database.js
+++ Pages/Activities/Database.js
@@ -10,16 +10,28 @@ const db = SQLite.openDatabase({
 db.transaction(tx => {
   tx.executeSql(
     'CREATE TABLE IF NOT EXISTS Users (id INTEGER PRIMARY KEY AUTOINCREMENT, FirstName varchar(20), LastName varchar(20), email varchar(30), password varchar(15), phone varchar(11), account varchar(15))',
-  //  'CREATE TABLE IF NOT EXISTS Payment (id INTEGER PRIMARY KEY AUTOINCREMENT, FirstName TEXT, LastName text, email TEXT, password text, phone text, account text)',
     [],
     (sqlTnx, reg)=>{
       console.log("Table has been created successful");
     },
-    
+
     error=>{
       console.log("Error in creating table"+error.message);
     },);
 });
+
+// db.transaction(tx => {
+//   tx.executeSql(
+//     'CREATE TABLE IF NOT EXISTS Payment (id INTEGER PRIMARY KEY AUTOINCREMENT, senderEmail varchar(30), receiverEmail varchar(30), amount decimal(5,2), createDate datetime, paymentdate datetime, status varchar(10))',
+//     [],
+//     (sqlTnx, reg)=>{
+//       console.log("Table has been created successful");
+//     },
+
+//     error=>{
+//       console.log("Error in creating table"+error.message);
+//     },);
+// });
 
 // Insert data into the table
 const addUsers=()=>{
@@ -27,22 +39,38 @@ const addUsers=()=>{
     alert("Enter user's information");
     return false;
   }
-
-
+  db.transaction(tx => {
+    tx.executeSql('INSERT INTO Users (name, email) VALUES (?,?,?,?,?,?,?,?)', [userID, firstName,lastName,gender,email,password,phone,account]);
+  (sqlTnx,reg)=>{
+     console.log('${firstName} has been added successful');
+     getFirstName();
+     setFirstName("");
+  },
+  error=>{
+    console.log("error on adding a user " + error.message);
+  }});
 }
 
-db.transaction(tx => {
-  tx.executeSql('INSERT INTO Users (name, email) VALUES (?,?,?,?,?,?,?,?)', [userID, firstName,lastName,gender,email,password,phone,account]);
-(sqlTnx,reg)=>{
-   console.log('${firstName} has been added successful');
-   getFirstName();
-   setFirstName("");
-},
-error=>{
-  console.log("error on adding a user " + error.message);
-}
+// const addPayments=()=>{
+//   if(!userID||!firstName||!lastName||!gender||!email||!password||!phone||!account){
+//     alert("Enter user's information");
+//     return false;
+//   }
+//   db.transaction(tx => {
+//     tx.executeSql('INSERT INTO Users (name, email) VALUES (?,?,?,?,?,?,?,?)', [userID, firstName,lastName,gender,email,password,phone,account]);
+//   (sqlTnx,reg)=>{
+//      console.log('${firstName} has been added successful');
+//      getFirstName();
+//      setFirstName("");
+//   },
+//   error=>{
+//     console.log("error on adding a user " + error.message);
+//   }});
+// }
 
-});
+
+
+
 
 // Fetch data from the table
 db.transaction(tx => {
